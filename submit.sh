d646f29d46681e1191de87e35acc7b8c9141993e
karanezi <karanezi@learn.chtc.wisc.edu>
2024-04-21 19:59:24 -0500

    changed names

---
+++ submit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+link=$1
+
+#destination_dir="./listings"
+#mkdir -p "$destination_dir"
+
+country=$(echo "$link" | awk -F'/' '{print $4}')
+city=$(echo "$link" | awk -F'/' '{print $6}')
+
+filename="${country}_${city}.csv"
+gz_file_path="${country}_${city}.csv.gz"
+#csv_file_path="$destination_dir/$filename"
+
+wget -c -O "$gz_file_path" "$link"
+
+gzip -d -c "$gz_file_path" > "$filename"
+
+rm "$gz_file_path"
+
